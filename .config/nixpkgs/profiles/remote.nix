4aecbc03eddb85ef326ceb20350e491227c2745b
kvwu <kvwu@transienterror.com>
2020-11-04 11:20:52 -0800

    add profiles

---
+++ .config/nixpkgs/profiles/remote.nix
@@ -0,0 +1,9 @@
+{
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    extraConfig = builtins.readFile extraConfigs/.tmux.conf;
+
+    plugins = with pkgs.tmuxPlugins; [ yank ];
+  };
+}
