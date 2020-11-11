73d4acb6e13560770efbf1bb9c537bcce9f51974
kvwu <kvwu@amazon.com>
2020-11-11 22:53:34 +0000

    [nix] add powerline to remote

--- .config/nixpkgs/profiles/remote.nix
+++ .config/nixpkgs/profiles/remote.nix
@@ -1,9 +1,15 @@
-{pkgs, ...}:
-{
+{ pkgs, lib, ... }:
+with lib; {
+  home.packages = with pkgs; [ powerline ];
+
   programs.tmux = {
     enable = true;
     clock24 = true;
-    extraConfig = builtins.readFile ../extraConfigs/.tmux.conf;
+    extraConfig = let
+      sources = [ "${pkgs.powerline}/share/tmux/powerline.conf" ];
+      sourcesString =
+        pipe sources [ (map (p: "source " + p)) (concatStringsSep "\n") ];
+    in sourcesString + "\n" + builtins.readFile ../extraConfigs/.tmux.conf;
 
     plugins = with pkgs.tmuxPlugins; [ yank ];
   };
