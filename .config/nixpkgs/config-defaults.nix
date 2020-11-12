fd9c1204008eef88ebdba18261afae6a556b0295
kvwu <kvwu@amazon.com>
2020-11-12 00:13:26 +0000

    [nix] fix powerline

--- .config/nixpkgs/config-defaults.nix
+++ .config/nixpkgs/config-defaults.nix
@@ -1,4 +1,5 @@
 {
     role = "";
     profiles = [];
+    powerlineTmuxSrc = "";
 }
--- .config/nixpkgs/profiles/remote.nix
+++ .config/nixpkgs/profiles/remote.nix
@@ -1,12 +1,28 @@
 { pkgs, lib, ... }:
-with lib; {
-  home.packages = with pkgs; [ powerline reptyr ];
+with lib; 
+let
+  defaultConfig = import ../config-defaults.nix;
+  configPath = ../config.nix;
+  myConfig = if builtins.pathExists configPath then
+    defaultConfig // import configPath
+  else
+    defaultConfig;
+in
+{
+  home.packages = with pkgs; [
+    reptyr
+  ];
+
+  home.file.".config/powerline" = {
+    source = ../extraConfigs/powerline;
+    recursive = true;
+  };
 
   programs.tmux = {
     enable = true;
     clock24 = true;
     extraConfig = let
-      sources = [ "${pkgs.powerline}/share/tmux/powerline.conf" ];
+      sources = [ myConfig.powerlineTmuxSrc ];
       sourcesString =
         pipe sources [ (map (p: "source " + p)) (concatStringsSep "\n") ];
     in sourcesString + "\n" + builtins.readFile ../extraConfigs/.tmux.conf;
