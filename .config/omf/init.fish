f1e01acad1e7653caba19b0f16037594afee1fc1
kvwu <kvwu@transienterror.com>
2020-11-24 09:24:09 -0800

    [fish] re-order init st starship is in the path

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,8 +1,8 @@
-starship init fish | source
 if test -z "$ZSH_AUTO_RAN_FISH" 
   bass '. $HOME/.nix-profile/etc/profile.d/nix.sh'
   bass '. $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh'
 end
+starship init fish | source
 
 if type -q direnv
   eval (direnv hook fish)
