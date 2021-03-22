b70a8703cf651b8fd3b50f0b76e0c92973150e95
kvwu <kvwu@transienterror.com>
2021-03-22 09:14:48 -0700

    reformat

--- .config/nixpkgs/os/darwin.nix
+++ .config/nixpkgs/os/darwin.nix
@@ -2,7 +2,5 @@
   home.file.".config/kitty/kitty-os.conf".source =
     ../extraConfigs/kitty/kitty-darwin.conf;
 
-  programs.fish = {
-    enable = true;
-  }; 
+  programs.fish = { enable = true; };
 }
