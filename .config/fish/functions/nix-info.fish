7ebe8c6904d7b94e7396ae81014bed35a5c21001
kvwu <kvwu@transienterror.com>
2021-03-22 08:34:40 -0700

    [nix] add more fields to nix info

--- .config/fish/functions/nix-info.fish
+++ .config/fish/functions/nix-info.fish
@@ -1,4 +1,5 @@
 function nix-info
-  nix-env -qa --json $argv | jq 'to_entries[] | {key: .key, name: .value.meta.name, homepage:.value.meta.homepage}'
+  nix-env -qa --json $argv | \
+  jq 'to_entries[] | {key: .key, name: .value.meta.name, homepage:.value.meta.homepage, license: .value.meta.license.fullName, platforms: .value.meta.platforms}'
 end
 
