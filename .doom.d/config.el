b6ca5aacf76eb2366a92e8d1ce31298e2544f988
kvwu <kvwu@amazon.com>
2021-04-12 11:59:17 -0700

    Add custom org mode states

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -81,7 +81,41 @@
   :defer t
   :config
   (setq! org-log-done 'time
-         org-agenda-start-with-log-mode t))
+         org-agenda-start-with-log-mode t
+         org-todo-keywords
+        '((sequence
+           "TODO(t)"  ; A task that needs doing & is ready to do
+           "PROJ(p)"  ; A project, which usually contains other tasks
+           "IN-PROGRESS(s)"  ; A task that is in progress
+           "REVIEW(r)"  ; A task that is in progress
+           "BLOCKED(b)"  ; Something external is holding up this task
+           "HOLD(h)"  ; This task is paused/on hold because of me
+           "IDEA(i)"  ; An unconfirmed and unapproved task or notion
+           "|"
+           "DONE(d)"  ; Task successfully completed
+           "KILL(k)"
+           "HAND-OFF(h)") ; Task was cancelled, aborted or is no longer applicable
+          (sequence
+           "[ ](T)"   ; A task that needs doing
+           "[-](S)"   ; Task is in progress
+           "[?](W)"   ; Task is being held up or paused
+           "|"
+           "[X](D)")  ; Task was completed
+          (sequence
+           "|"
+           "OKAY(o)"
+           "YES(y)"
+           "NO(n)"))
+        org-todo-keyword-faces
+        '(("[-]"  . +org-todo-active)
+          ("STRT" . +org-todo-active)
+          ("[?]"  . +org-todo-onhold)
+          ("BLOCKED" . +org-todo-onhold)
+          ("HOLD" . +org-todo-onhold)
+          ("REVIEW" . +org-todo-onhold)
+          ("PROJ" . +org-todo-project)
+          ("NO"   . +org-todo-cancel)
+          ("KILL" . +org-todo-cancel))))
 
 (after! text-mode
   (when (executable-find "aspell")
