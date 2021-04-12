f919feb6367a8b1b1995000b7b1410c90304adc8
kvwu <kvwu@amazon.com>
2021-04-12 10:00:04 -0700

    Add messages to gitptr functions

--- .doom.d/config-manager.el
+++ .doom.d/config-manager.el
@@ -3,10 +3,11 @@
 (defun set-gitptr ()
   (interactive)
   (let ((home (getenv "HOME")))
-        (rename-file (expand-file-name ".gitptr" home) (expand-file-name ".git" home))))
+    (rename-file (expand-file-name ".gitptr" home) (expand-file-name ".git" home))
+    (message "successfully set gitpr")))
 
 (defun unset-gitptr ()
   (interactive)
   (let ((home (getenv "HOME")))
-    (rename-file (expand-file-name ".git" home) (expand-file-name ".gitptr" home))))
-
+    (rename-file (expand-file-name ".git" home) (expand-file-name ".gitptr" home))
+    (message "successfully unset gitpr")))
