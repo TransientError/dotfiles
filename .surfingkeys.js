867816b49bd903e42810a54fddb78b29cdb2cb60
kvwu <kvwu@transienterror.com>
2021-04-19 02:04:15 -0700

    [surfingkeys] Try 2x faster scrolling

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -9,6 +9,8 @@ map('gt', 'T');
 // an example to remove mapkey `Ctrl-i`
 unmap('<Ctrl-i>');
 
+settings.scrollStepSize = 140;
+
 // ---- Hints ----
 // Hints have to be defined separately
 // Uncomment to enable
