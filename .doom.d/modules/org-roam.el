3edb19bed08a116ef6d59a5da8d65d604b55f534
kvwu <kvwu@DESKTOP-GUGNFQV.localdomain>
2021-11-01 13:54:30 -0700

    [doom] Add some templates for work

--- .doom.d/modules/org-roam.el
+++ .doom.d/modules/org-roam.el
@@ -22,26 +22,29 @@
   :init
   (setq! org-roam-v2-ack t)
   :config
-  (setq! org-roam-directory (file-truename org-directory)
-         org-roam-capture-templates
-         '(("d" "default" plain "%[/home/kvwu/Dropbox/templates/general.org]"
-            :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: \n")
-            :unnarrowed t)
-           ("r" "restaurants" plain "%[/home/kvwu/Dropbox/templates/restaurants.org]"
-            :target (file+head "restaurants/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: :restaurant:\n")
-            :unnarrowed t
-            :empty-lines-after 1)
-           ("c" "cinema-therapy" plain "%[/home/kvwu/Dropbox/templates/cinema-therapy.org]"
-            :target (file+head "%<%Y-%m-%d>-ct-${slug}.org" "#+title: ${title}\n'")
-            :unnarrowed t)
-           ("t" "todo" entry "* TODO %?" :target (file+olp "%<%Y-%m-%d>.org" ("Todo")) :unnarrowed t)
-           ("m" "meetings" plain "%[~/Dropbox/templates/meetings.org]"
-            :target (file+head "%<%Y-%m-%d>-ct-${slug}.org" "#+title: ${title}\n'")
-            :unnarrowed t
-            :empty-lines-after 1))
-         org-roam-dailies-capture-templates
-         '(("d" "default" plain "%[/home/kvwu/Dropbox/templates/journal.org]"
-            :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  (if (personal-config-has-profile 'work)
+      (setq! org-roam-capture-templates
+             '(("d" "default" plain ""
+                :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: \n") :unnarrowed t)
+               ("m" "meetings" plain ""
+                :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"  "#+title: ${title}\n#+filetags: \n") :unnarrowed t)))
+    (setq! org-roam-capture-templates
+           '(("d" "default" plain  "%[/home/kvwu/Dropbox/templates/general.org]"
+              :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: \n")
+              :unnarrowed t)
+             ("r" "restaurants" plain "%[/home/kvwu/Dropbox/templates/restaurants.org]"
+              :target (file+head "restaurants/%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: :restaurant:\n")
+              :unnarrowed t
+              :empty-lines-after 1)
+             ("c" "cinema-therapy" plain "%[/home/kvwu/Dropbox/templates/cinema-therapy.org]"
+              :target (file+head "%<%Y-%m-%d>-ct-${slug}.org" "#+title: ${title}\n'")
+              :unnarrowed t)
+             ("t" "todo" entry "* TODO %?" :target (file+olp "%<%Y-%m-%d>.org" ("Todo")) :unnarrowed t)
+             ("m" "meetings" plain "%[~/Dropbox/templates/meetings.org]"
+              :target (file+head "%<%Y-%m-%d>-ct-${slug}.org" "#+title: ${title}\n'")
+              :unnarrowed t
+              :empty-lines-after 1))))
+  (setq! org-roam-directory (file-truename org-directory))
   (org-roam-db-autosync-mode))
 
 (use-package! websocket
