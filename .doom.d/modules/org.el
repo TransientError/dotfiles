2d00b94eeea81f627caaaa03e5f7c1e66ef164c9
kvwu <kvwu@DESKTOP-GUGNFQV.localdomain>
2021-11-03 16:23:27 -0700

    [doom] Adds some work functionality

--- .doom.d/modules/org.el
+++ .doom.d/modules/org.el
@@ -1,12 +1,32 @@
 ;;; org.el -*- lexical-binding: t; -*-
 
-(unless (personal-config-has-profile 'work)
-  (map! :leader "X" nil ;; unmap org-capture because I use roam
-        (:prefix ("X" . "quick open")
-         :desc "open refile" "r" (lambda () (interactive) (find-file "~/Dropbox/todo.org"))
-         :desc "open habits" "h"
-         (lambda () (interactive)
-           (org-roam-node-visit (org-roam-node-from-title-or-alias (format-time-string "%Y-%m-habits")))))))
+(setq-local refilePath (if (personal-config-has-profile 'work) "~/org-roam/refile.org" "~/Dropbox/todo.org"))
+
+(defun ndk/org-set-effort-in-pomodoros (&optional n)
+  "This `fn` facilitate to define efforts in tasks in pomodoros units.
+You can use interactively by typing `C-c C-x e` or by sending parameter as `M-3 C-c C-x e`."
+  (interactive "P")
+  (setq n (or n (string-to-number (read-from-minibuffer "How many pomodoros? " nil nil nil nil "1" nil))))
+  (let* ((mins-per-pomodoro (if org-pomodoro-length
+                                org-pomodoro-length
+                              25)))
+    (org-set-effort nil (org-duration-from-minutes (* n mins-per-pomodoro)))))
+
+(map! :leader "X" nil ;; unmap org-capture because I use roam
+      (:prefix ("X" . "quick open")
+       :desc "open refile" "r" (lambda () (interactive) (find-file refilePath))
+       (:unless (personal-config-has-profile 'work)
+        :desc "open habits" "h"
+        (lambda () (interactive)
+          (org-roam-node-visit (org-roam-node-from-title-or-alias (format-time-string "%Y-%m-habits")))))
+       (:when (personal-config-has-profile 'work)
+        :desc "open todo" "t" (lambda () (interactive) (find-file "~/org-roam/todo.org"))
+        :desc "open personal" "p" (lambda () (interactive) (find-file "~/org-roam/personal.org"))))
+
+      :desc "estimate pomodoros" "m c E" #'ndk/org-set-effort-in-pomodoros)
+
+
+
 
 (use-package! org
   :defer t
