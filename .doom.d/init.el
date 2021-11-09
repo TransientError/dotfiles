f57bf7a2a21ad29a961409094102ec8f517962a4
kvwu <kvwu@transienterror.com>
2021-11-08 15:10:43 -1000

    [doom] bugfix

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -148,7 +148,7 @@
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
-       (:if (personal-config-has-profile 'work) ledger)            ; an accounting system in Emacs
+       (:unless (personal-config-has-profile 'work) ledger)            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
