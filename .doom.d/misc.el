ff20f69efee7b46d419dd4de79e7b4cc71c33b19
kvwu <kvwu@transienterror.com>
2021-11-24 02:24:59 -0800

    [doom] add a function to get memory usage

--- .doom.d/misc.el
+++ .doom.d/misc.el
@@ -14,3 +14,14 @@
 
 (defun kvwu/is-wsl ()
   (when (executable-find "uname") (string-match-p "-[Mm]icrosoft" (shell-command-to-string "uname -a"))))
+
+(defun ap/garbage-collect ()
+  "Run `garbage-collect' and print stats about memory usage."
+  (interactive)
+  (message (cl-loop for (type size used free) in (garbage-collect)
+                    for used = (* used size)
+                    for free = (* (or free 0) size)
+                    for total = (file-size-human-readable (+ used free))
+                    for used = (file-size-human-readable used)
+                    for free = (file-size-human-readable free)
+                    concat (format "%s: %s + %s = %s\n" type used free total))))
