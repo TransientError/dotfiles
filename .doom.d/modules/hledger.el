63c2222b8539486035ab915ec91b64ed18ca7d4a
kvwu <kvwu@transienterror.com>
2022-05-15 05:43:53 -0700

    [hledger] fix indentation

--- .doom.d/modules/hledger.el
+++ .doom.d/modules/hledger.el
@@ -13,22 +13,23 @@
          hledger-currency-string "$")
   (evil-make-intercept-map hledger-view-mode-map)
   (map!
-   (:after ledger-mode :map hledger-mode-map)
-   :localleader "f" #'ledger-mode-clean-buffer
-   (:after hledger-mode :map hledger-mode-map)
-   :localleader "d" #'hledger-run-command
-   :localleader "p" (defun kvwu/hledger-print ()
-                      (interactive)
-                      (let* ((inhibit-read-only t)) (hledger-jdo "print --auto")))
-   :localleader "r" (defun kvwu/hledger-balance ()
-                      (interactive)
-                      (let* ((inhibit-read-only t))
-                        (output-buffer (hledger-jdo "accounts -f" nil t))
-                        (accounts (split-string (with-current-buffer output-buffer (buffer-string))))
-                        (ivy-read "" accounts :action
-                                  (lambda (account) (hledger-jdo (format "register %s --auto" account)))))))
+   (:after ledger-mode :map hledger-mode-map
+    :localleader "f" #'ledger-mode-clean-buffer)
+   (:after hledger-mode :map hledger-mode-map
+    :localleader "d" #'hledger-run-command
+    :localleader "p" (defun kvwu/hledger-print ()
+                       (interactive)
+                       (let* ((inhibit-read-only t)) (hledger-jdo "print --auto")))
+    :localleader "r" (defun kvwu/hledger-balance ()
+                       (interactive)
+                       (let* ((inhibit-read-only t)
+                              (output-buffer (hledger-jdo "accounts" nil t))
+                              (accounts (split-string (with-current-buffer output-buffer (buffer-string)))))
+                         (ivy-read "" accounts :action
+                                   (lambda (account) (hledger-jdo (format "register %s --auto" account)))))))))
 
-  (use-package! flycheck-hledger
-    :when (featurep! :checkers syntax)
-    :after hledger-mode
-    :demand t))
+
+(use-package! flycheck-hledger
+  :when (featurep! :checkers syntax)
+  :after hledger-mode
+  :demand t)
