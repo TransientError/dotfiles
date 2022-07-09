f126de68ba22cc856494a1e7f6c92e60d6867c25
kvwu <kvwu@transienterror.com>
2022-07-09 06:34:42 -0700

    [mail] add protonmail

--- .doom.d/modules/mail.el
+++ .doom.d/modules/mail.el
@@ -12,18 +12,32 @@
                         (mu4e-drafts-folder . "/gmail/[Gmail]/Drafts")
                         (mu4e-trash-folder . "/gmail/[Gmail]/Trash")
                         (mu4e-refile-folder . "/gmail/[Gmail]/All Mail")
-                        (smtpmail-smtp-user . "kgqw503"))
+                        (smtpmail-smtp-user . "kgqw503")
+                        (smtpmail-smtp-server . "smtp.gmail.com")
+                        (smtpmail-smtp-service . 587))
                       t)
+  (set-email-account! "protonmail"
+                      '((mu4e-sent-folder . "/protonmail/Sent")
+                        (mu4e-drafts-folder . "/protonmail/Drafts")
+                        (mu4e-trash-folder . "/protonmail/Trash")
+                        (mu4e-refile-folder . "/protonmail/Archive")
+                        (smtpmail-smtp-user . "kev.wu")
+                        (smtpmail-smtp-server . "127.0.0.1")
+                        (smtpmail-stream-type . plain)
+                        (user-mail-address . "kev.wu@protonmail.com")
+                        (smtpmail-smtp-service . 1025))
+                      nil)
+
   (setq! mu4e-view-html-plaintext-ratio-heuristic most-positive-fixnum
          mu4e-view-prefer-html nil
-         mail-user-agent #'mu4e-user-agent)
+         mail-user-agent #'mu4e-user-agent
+         mu4e-compose-context-policy 'ask-if-none
+         smtpmail-servers-requiring-authorization "127.0.0.1")
 
   (cond (IS-MAC (setq! message-send-mail-function 'sendmail-send-it sendmail-program (executable-find "msmtp")))
         (IS-LINUX (setq! message-send-mail-function 'smtpmail-send-it
                          smtpmail-local-domain "gmail.com"
                          smtpmail-default-smtp-server "smtp.gmail.com"
-                         smtpmail-smtp-server "smtp.gmail.com"
-                         smtpmail-smtp-service 587
                          browse-url-browser-function #'browse-url-chrome)))
 
   (add-to-list 'mm-discouraged-alternatives "text/html")
@@ -31,6 +45,9 @@
         '((:name "Inbox"
            :query "maildir:/gmail/Inbox and not flag:trashed"
            :key ?i)
+          (:name "Protonmail Inbox"
+           :query "maildir:/protonmail/Inbox and not flag:trashed"
+           :key ?p)
           (:name "Unread messages"
            :query "flag:unread and not flag:trashed and not flag:seen and maildir:/gmail/Inbox"
            :key ?u))))
