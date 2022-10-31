9c8654269332a1543d9561db7e85d7fba69b6445
kvwu <kvwu@transienterror.com>
2022-10-31 15:04:24 -0700

    reactivate js and rust

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -139,7 +139,7 @@
        ;;idris             ; a language you can depend on
        (json +lsp)              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;; javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -162,7 +162,7 @@
        rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;; (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        (sh +fish)                ; she sells {ba,z,fi}sh shells on the C xor
--- .doom.d/modules/javascript.el
+++ .doom.d/modules/javascript.el
@@ -8,3 +8,6 @@
   :config
   (setq! web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))
          web-mode-markup-indent-offset 2))
+
+;; typescript
+(setq-hook! typescript-mode-hook tab-width 2 typescript-indent-level 2)
