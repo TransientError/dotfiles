5b342f271c2ad91dd1b7d02731d7df04cf8e8b22
kvwu <kvwu@transienterror.com>
2022-12-07 02:23:09 -0800

    hydra for debug!

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -205,7 +205,39 @@ return require("packer").startup(function(use)
       vim.keymap.set("n", "<leader>dn", dap.step_over)
       vim.keymap.set("n", "<leader>dsi", dap.step_into)
       vim.keymap.set("n", "<leader>dx", dap.repl.open)
+
+      local hydra = require "hydra"
+      local hint = [[
+         _n_: step over   _c_: Continue/Start   _b_: Breakpoint     
+         _i_: step into   _X_: Quit                                
+         _o_: step out    _q_: exit
+        ]]
+
+      hydra {
+        hint = hint,
+        name = "dap",
+        mode = { "n", "x" },
+        body = "<leader>dh",
+        heads = {
+          { "n", dap.step_over, { silent = true } },
+          { "i", dap.step_into, { silent = true } },
+          { "o", dap.step_out, { silent = true } },
+          { "c", dap.continue, { silent = true } },
+          { "b", dap.toggle_breakpoint, { silent = true } },
+          { "X", dap.close, { silent = true } },
+          { "q", nil, { exit = true, nowait = true } },
+        },
+        config = {
+          color = 'pink',
+          invoke_on_body = true,
+          hint = {
+            position = 'bottom',
+            border = 'rounded'
+          }
+        }
+      }
     end,
+    requires = { "anuvyklack/hydra.nvim" },
   }
   use {
     "mxsdev/nvim-dap-vscode-js",
