d28ef5a671f205a3be4558fcbd071edfee9aea62
kvwu <kvwu@transienterror.com>
2023-03-08 17:11:54 -0800

    autoclose tag for neovim

--- .config/nvim/lua/kvwu-misc-old-crap.lua
+++ .config/nvim/lua/kvwu-misc-old-crap.lua
@@ -9,14 +9,15 @@ function kvwu_crap.setup(use, not_vscode)
   use { "jparise/vim-graphql", ft = "graphql" }
   use { "cespare/vim-toml", ft = "toml" }
   use { "dag/vim-fish", ft = "fish" }
-  use { "mattn/emmet-vim", ft = { "html", "jsx" } }
-  use {
-    "alvan/vim-closetag",
-    config = function()
-      vim.g.closetag_filenames = "*.html,*.xml,*.plist,*.*proj"
-    end,
-    ft = { "html", "xml" },
-  }
+  use { "mattn/emmet-vim", ft = { "html", "jsx", "xml" } }
+  -- use {
+  --   "alvan/vim-closetag",
+  --   config = function()
+  --     vim.g.closetag_filenames = "*.html,*.xml,*.plist,*.*proj"
+  --     vim.g.closetag_filetypes = "xml, html"
+  --   end,
+  --   ft = { "html", "xml" },
+  -- }
 end
 
 return kvwu_crap
