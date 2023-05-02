5a7dc03fcb3fe096cc3a7bb402246c82c0d8da9b
kvwu <wukevin@microsoft.com>
2023-05-02 10:43:44 -0700

    support scoop chrome

--- .doom.d/misc.el
+++ .doom.d/misc.el
@@ -16,10 +16,13 @@
   (when (executable-find "uname") (string-match-p "-[Mm]icrosoft" (shell-command-to-string "uname -a"))))
 
 (defun kvwu/wsl-browser (browser)
-  (let ((program-files-path "/mnt/c/Program Files/"))
+  (let* ((program-files-path "/mnt/c/Program Files/")
+         (windows-home "/mnt/c/Users/wukevin/")
+         (scoop-bin (concat windows-home "scoop/shims/")))
     (pcase browser
       ('firefox (concat program-files-path "/Mozilla Firefox/firefox.exe"))
-      ('chrome (concat program-files-path "Google/Chrome/Application/chrome.exe")))))
+      ('chrome (concat program-files-path "Google/Chrome/Application/chrome.exe"))
+      ('scoop-chrome (concat scoop-bin "chrome.exe")))))
 
 (defun ap/garbage-collect ()
   "Run `garbage-collect' and print stats about memory usage."
