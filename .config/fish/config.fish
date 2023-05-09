570a54b002630c4f32ac2a01f9856602cde5b2e2
kvwu <kvwu@transienterror.com>
2023-05-08 17:37:44 -0700

    fix pathing on macos

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,10 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   if test -z "$MSYSTEM" 
-    if test -d "$HOME/.nix-profile/etc/profile.d"
-      if test -e "$HOME/.nix-profile/etc/profile.d/nix.fish"
+    if test -d "$HOME/.nix-profile/etc/profile.d/nix.fish"
         source "$HOME/.nix-profile/etc/profile.d/nix.fish"
-      else
+    else if test -e "$HOME/.nix-profile/etc/profile.d/nix.sh"
         bass ". $HOME/.nix-profile/etc/profile.d/nix.sh"
-      end
     else if test -d '/nix/var/nix/profiles/default/etc/profile.d/'
       source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
     end
