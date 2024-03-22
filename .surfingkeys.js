be60eff38139c486b5da50eab3d40bafe7bbfe53
kvwu <kvwu@transienterror.com>
2024-03-22 00:27:05 -0700

    Change default search engine

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -2,6 +2,7 @@ settings.scrollStepSize = 175;
 
 removeSearchAlias("b");
 addSearchAlias("b", "Brave", "https://search.brave.com/search?q=", "s", "https://search.brave.com/suggest?q=");
+settings.defaultSearchEngine = "b";
 
 api.mapkey(";dI", "#1Download image to", function () {
   api.Hints.create("img", function (element) {
