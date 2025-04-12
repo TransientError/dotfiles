530bc559709ca4dbcdfcc636b3a52b5f96db35d8
kvwu <me@kvwu.io>
2025-04-12 00:36:54 -0700

    fix rainbow delimeter

--- .config/nvim/lua/plugins/kvwu-treesitter.lua
+++ .config/nvim/lua/plugins/kvwu-treesitter.lua
@@ -3,32 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
     module = { "nvim-treesitter.query", "nvim-treesitter.configs" },
-  },
-  {
-    "p00f/nvim-ts-rainbow",
-    config = function()
-      local colors = require "material.colors"
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "python", "lua", "typescript" },
-        highlight = {
-          enable = true,
-        },
-        rainbow = {
-          enable = true,
-          colors = {
-            colors.main.purple,
-            colors.main.yellow,
-            colors.main.blue,
-            colors.main.cyan,
-            colors.main.green,
-            colors.main.orange,
-            colors.main.red,
-          },
-        },
-      }
-
-      vim.treesitter.language.register("xml", "html")
-    end,
   }
 }
 
