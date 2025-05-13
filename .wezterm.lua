b24bae7ae337815cef52522ff63c15afe07ed168
kvwu <kvwu@transienterror.com>
2025-05-13 05:43:35 -0700

    wezterm: fix bugs

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,10 +2,14 @@ local wezterm = require "wezterm" --[[@as Wezterm]]
 local config = wezterm.config_builder()
 
 local tabline = wezterm.plugin.require "https://github.com/michaelbrusegard/tabline.wez"
+tabline.setup { options = { theme = 'Tomorrow Night Bright' } } ---@diagnostic disable-line: undefined-field
+tabline.apply_to_config(config)
+config.tab_bar_at_bottom = true
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "pwsh" }
   config.color_scheme = "Campbell (Gogh)"
+  config.window_decorations = "TITLE | RESIZE"
 
   config.launch_menu = {
     {
@@ -27,7 +31,11 @@ config.keys = {
     key = "c",
     mods = "CTRL",
     action = wezterm.action_callback(function(window, pane)
-      local selection_text = window:get_selection_text_for_pane()
+      if pane == nil then
+        wezterm.log_error('pane is nil')
+        return
+      end
+      local selection_text = window:get_selection_text_for_pane(pane)
       local is_selection_active = string.len(selection_text) ~= 0
       if is_selection_active then
         window:perform_action(wezterm.action.CopyTo "ClipboardAndPrimarySelection", pane)
@@ -58,7 +66,4 @@ config.keys = {
   },
 }
 
-tabline.setup { options = { theme = 'Tomorrow Night Bright' } }
-tabline.apply_to_config(config)
-config.tab_bar_at_bottom = true
 return config
