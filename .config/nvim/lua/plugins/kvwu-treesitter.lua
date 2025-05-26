97deb5a5edf9e6d21a7ee8dab3d71f8ccb2e4a95
kvwu <me@kvwu.io>
2025-05-26 00:00:33 -0700

    nvim: optimize treesitter

--- .config/nvim/lua/plugins/kvwu-treesitter.lua
+++ .config/nvim/lua/plugins/kvwu-treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "LazyFile", "VeryLazy" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
     opts = {
       highlight = {
         enable = true,
