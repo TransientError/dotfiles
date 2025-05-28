ce6cec133359270257677368288f1ffe72951c7f
kvwu <kvwu@transienterror.com>
2025-05-27 21:30:50 -0700

    yazi: add some plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,3 +1,8 @@
 require("bookmarks"):setup {
   last_directory = { enable = true, persist = false, mode = "dir" },
 }
+
+require("relative-motions"):setup {
+  show_numbers = 'relative',
+  show_motion = true,
+}
