558b6e0e7b9926b2c07ed16ddb0097afdb91cece
kvwu <me@kvwu.io>
2025-05-29 00:15:59 +0000

    nvim: fix refactor

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,48 +1,11 @@
 return {
   {
-    "echasnovski/mini.surround",
-    version = false,
-    opts = {
-      mappings = {
-        add = "ys",
-        delete = "ds",
-        replace = "cs",
-      },
-    },
-    keys = {
-      { "ys", "", desc = "add surround" },
-      { "ds", "", desc = "delete surround" },
-      { "cs", "", desc = "replace surround" },
-      {
-        "S",
-        function()
-          require("mini.surround").add "visual"
-        end,
-        mode = { "v" },
-        silent = true,
-      },
-    },
-    config = function(_, opts)
-      require("mini.surround").setup(opts)
-      vim.keymap.del("x", "ys")
-    end,
-  },
-  {
     "lambdalisue/suda.vim",
     cmd = { "SudaWrite" },
   },
   {
     "folke/which-key.nvim",
     opts = {},
-  },
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      local npairs = require "nvim-autopairs"
-      npairs.setup {}
-      local Rule = require "nvim-autopairs.rule"
-      npairs.add_rule(Rule("`", "`", "-ocaml"))
-    end,
   },
   {
     "chentoast/marks.nvim",
@@ -98,107 +61,4 @@ return {
   { "eraserhd/parinfer-rust", build = "cargo build --release", ft = "lisp" },
   { "b0o/SchemaStore", ft = { "json", "jsonc", "yaml" } },
   { "williamboman/mason.nvim", opts = {}, event = "VeryLazy" },
-  {
-    "stevearc/conform.nvim",
-    opts = { formatters_by_ft = { lua = { "stylua" } } },
-    event = "LazyFile",
-    keys = {
-      {
-        "<leader>cf",
-        function()
-          require("conform").format { async = true }
-        end,
-      },
-    },
-  },
-  {
-    "wellle/targets.vim",
-    event = "LazyFile",
-    init = function()
-      vim.cmd [[
-        autocmd User targets#mappings#user call targets#mappings#extend({
-           \ 'a': {'argument': [{'o':'[({<[]', 'c':'[]}>)]', 's': ','}]}
-           \ })
-     ]]
-    end,
-  },
-  {
-    "gbprod/substitute.nvim",
-    opts = {},
-    keys = {
-      {
-        "gr",
-        function()
-          require("substitute").operator()
-        end,
-        mode = "n",
-        noremap = true,
-      },
-    },
-  },
-  {
-    "aaronik/treewalker.nvim",
-    opts = {},
-    keys = {
-      {
-        "<leader>sl",
-        "<cmd>Treewalker SwapRight<cr>",
-        silent = true,
-      },
-      {
-        "<leader>sh",
-        "<cmd>Treewalker SwapLeft<cr>",
-        silent = true,
-      },
-      {
-        "<leader>sj",
-        "<cmd>Treewalker SwapDown<cr>",
-        silent = true,
-      },
-      {
-        "<leader>sk",
-        "<cmd>Treewalker SwapUp<cr>",
-        silent = true,
-      },
-    },
-  },
-  {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-      {
-        "s",
-        function()
-          require("flash").jump()
-        end,
-        mode = { "n", "x", "o" },
-        desc = "Flash",
-      },
-      {
-        "S",
-        function()
-          require("flash").treesitter()
-        end,
-        mode = { "n" },
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        function()
-          require("flash").remote()
-        end,
-        mode = { "o", "x" },
-        desc = "Flash Remote",
-      },
-      {
-        "R",
-        function()
-          require("flash").treesitter_search()
-        end,
-        mode = { "o", "x" },
-        desc = "Flash Treesitter Search",
-      },
-    },
-  },
 }
